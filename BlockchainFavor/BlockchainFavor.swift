041c215475066352758ca6ca7fcd00523fa1c200
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-04-21 18:39:37 +0300

    Initial Commit

---
+++ BlockchainFavor/BlockchainFavor.swift
@@ -0,0 +1,11 @@
+//
+//  BlockchainFavor.swift
+//  BlockchainFavor
+//
+//  Created by Pavlo Boiko on 21.04.18.
+//  Copyright © 2018 Pavlo Boiko. All rights reserved.
+//
+
+class BlockchainFavor {
+
+}
