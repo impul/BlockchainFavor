fe1dfb30380bfe61edea9e450960ba18d5681982
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-05-02 01:17:36 +0300

    Prepare network client

--- Sources/BlockchainFavor/Network/BlockchainNetworkClient.swift
+++ Sources/BlockchainFavor/Network/BlockchainNetworkClient.swift
@@ -8,15 +8,41 @@
 import Foundation
 import CocoaAsyncSocket
 
-class BlockchainNetworkClient {
+class BlockchainNetworkClient:NSObject, GCDAsyncSocketDelegate {
  
     let blockchainUrl:URL
-    let socket:GCDAsyncSocket
-    let socketDelegate = BlockchainSocketDelegate()
+    var socket:GCDAsyncSocket!
     
-    init(with url:URL) {
+    required init(with url:URL) {
         blockchainUrl = url
-        socket = GCDAsyncSocket(delegate: socketDelegate, delegateQueue: .main)
+        super.init()
+        socket = GCDAsyncSocket(delegate: self, delegateQueue: .main)
     }
     
+    //MARK: - Controls
+    
+    func connectToNetwork() throws {
+        guard socket.isDisconnected else { return }
+        try socket.connect(toHost: blockchainUrl.host ?? "", onPort: UInt16(blockchainUrl.port ?? 5555))
+    }
+    
+    func disconnect() {
+        guard socket.isConnected else { return }
+        socket.disconnect()
+    }
+    
+    //MARK: - GCDAsyncSocketDelegate
+    
+    func socket(_ sock: GCDAsyncSocket, didConnectToHost host: String, port: UInt16) {
+        
+    }
+    
+    func socketDidDisconnect(_ sock: GCDAsyncSocket, withError err: Error?) {
+        
+    }
+    
+    func socket(_ sock: GCDAsyncSocket, didRead data: Data, withTag tag: Int) {
+        
+    }
 }
+
